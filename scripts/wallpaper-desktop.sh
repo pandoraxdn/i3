b2000044a17d6ba293022e82a17f45f502233313
pandoraxdn <pandoraxdn@protonmail.com>
2022-02-13 00:07:48 -0600

    Update Wallpaper module in I3, create script of wallpaper and set in polybar

---
+++ scripts/wallpaper-desktop.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+#!/bin/bash
+log=`cat ~/.config/i3/scripts/.log-wallpaper`
+
+wallpaper="$HOME/Imágenes/wallpaper/91383415_p0.jpg"
+
+case $1 in
+
+    random)
+        aleatory=`find ~/Imágenes/wallpaper -type f | shuf -n 1`
+        cp /dev/null ~/.config/i3/scripts/.log-wallpaper
+        echo $aleatory > ~/.config/i3/scripts/.log-wallpaper
+        feh --bg-fill $aleatory --no-fehbg
+    ;;
+
+    default)
+        cp /dev/null ~/.config/i3/scripts/.log-wallpaper
+        echo $wallpaper > ~/.config/i3/scripts/.log-wallpaper
+        feh --bg-fill $wallpaper --no-fehbg
+    ;;
+
+
+    center)
+        feh --bg-center $log --no-fehbg
+    ;;
+
+    extended)
+        feh --bg-fill $log --no-fehbg
+    ;;
+
+    max)
+        feh --bg-max $log --no-fehbg
+    ;;
+
+    scale)
+
+        feh --bg-scale $log --no-fehbg
+    ;;
+
+    repeat)
+
+        feh --bg-tile $log --no-fehbg
+    ;;
+
+
+    *)
+        echo "No aplica el valor ingresado"
+
+    ;;
+
+esac
