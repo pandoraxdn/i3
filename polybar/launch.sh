fed33bb4af6d487cfcac4efde9204e2964757140
pandoraxdn <pandoraxdn@protonmail.com>
2022-08-21 18:30:28 -0500

    Update i3 Nixos

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,19 +1,6 @@
 
 dir="$HOME/.config/i3/polybar/cuts/config.ini"
 
-#
-
-#launch_bar() {
-
-	#killall -q polybar
-
-	#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-	#polybar -q top -c "$dir" &
-
-	#polybar -q bottom -c "$dir" &
-#}
-
 launch_bar() {
 
 	killall -q polybar
@@ -22,6 +9,7 @@ launch_bar() {
 
 	polybar -q top -c "$dir" &
 
+	polybar -q bottom -c "$dir" &
 }
 
 launch_bar
